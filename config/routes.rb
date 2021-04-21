5847909242cde85f9dcf8016db4a4bcdf90ccf11
Daniel <cdanielask@gmail.com>
2021-04-21 15:55:12 +1000

    Added some helpful comments to show how resources works

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,20 @@ Rails.application.routes.draw do
  get '/hello', to: 'students#hello', as: 'hello'
 
 #  post '/students', to: 'students#create'
- resources :students
+ resources :students,except: [:destroy, :edit, :update]
+# get '/students', to: 'students#index', as: 'students'
+
+# post '/students', to: 'students#create'
+
+# get '/students/new',  to: 'students#new', as: 'new_student'
+
+# get '/students/:id/edit', to: 'students#edit', as: 'edit_student'
+
+# get '/students/:id', to: 'students#show', as: 'student'
+
+# patch '/students/:id', to: 'students#update'
+# put '/students/:id', to: 'students#update'
+
+# delete '/students/:id', to: 'students#destroy'
  root 'students#index'
 end
